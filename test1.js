b760009471b99f3773c69d506708f28ea36a3b54
gtrfsec <104921988+gtrfsec@users.noreply.github.com>
2022-05-04 17:46:33 +0100

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,2 +1,2 @@
 alert("Hello from Javascript for domain "+document.domain);
-document.getElementById('support_email').href="badguy@badguy.com";
+document.getElementById('support_email').href="mailto:badguy@badguy.com";
